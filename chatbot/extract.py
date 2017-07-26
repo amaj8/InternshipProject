8168f08c0edac6d0b883af511558440a6f3e40ff
amaj8 <aaloshree@gmail.com>
2017-07-26 20:05:11 +0530

    xvfb chrome

--- chatbot/extract.py
+++ chatbot/extract.py
@@ -11,7 +11,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()  
 chrome_options.add_argument("--headless")  
 """
-"""
+
 GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 chrome_options = Options()
@@ -19,7 +19,7 @@ chrome_options.binary_location = GOOGLE_CHROME_BIN
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-"""
+
 #filter strings to be used in Flipkart's url
 
 
@@ -251,7 +251,7 @@ class Flip:
 		#Running Chrome headless using xvfb and pyvirtualdisplay
 		display = Display(visible=0, size=(800, 600))
 		display.start()
-		driver = webdriver.Chrome()
+		#driver = webdriver.Chrome()
 
 
 		driver.get(url)
