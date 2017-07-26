3383a4bd611b3cacf1b7ed1d9f1dd05a9b26079a
amaj8 <aaloshree@gmail.com>
2017-07-26 21:00:40 +0530

    Login

--- website/settings.py
+++ website/settings.py
@@ -145,3 +145,13 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+LOGIN_REDIRECT_URL = 'login_success'
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'shopping.chatbot@gmail.com'
+EMAIL_HOST_PASSWORD = '1234abcd@'
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = 'shopping.chatbot@gmail.com'
--- website/urls.py
+++ website/urls.py
@@ -17,8 +17,19 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
+from chatbot import views as core_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include("chatbot.urls", namespace="chatbot"))
+    url(r'^$', core_views.home , name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'login_success/$', core_views.login_success, name='login_success'),
+    url(r'^login_success/chatbot/',include("chatbot.urls", namespace="chatbot")),
+
+    url(r'^signup/account_activation_sent/$', core_views.account_activation_sent, name='account_activation_sent'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        core_views.activate, name='activate'),
 ]
