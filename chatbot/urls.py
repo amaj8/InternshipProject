1eb24fa5534a99f0b3d8a6535e22c0382ae7c5cc
amaj8 <aaloshree@gmail.com>
2017-07-26 20:51:17 +0530

    xvfb chrome

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -10,4 +10,6 @@ from . import views
 urlpatterns = [
     url(r'^$', run_script, name='run_script'),
     url(r'^final_response/$', views.final_response, name='final_response'),
+     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+                views.activate, name='activate'),
 ]
